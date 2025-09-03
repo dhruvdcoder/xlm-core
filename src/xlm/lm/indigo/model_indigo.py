7a19083981db5c77352d7f5a57dc83e8ac2d6ab7
Vihaan Sejwani <vsejwani_umass_edu@gypsum-gpu153.unity.rc.umass.edu>
2025-09-02 22:16:40 -0400

    added scafollding for model_indigo.py

--- src/xlm/lm/indigo/model_indigo.py
+++ src/xlm/lm/indigo/model_indigo.py
@@ -32,10 +32,34 @@ logger = RankedLogger(__name__, rank_zero_only=True)
 class IndigoModel(nn.Module):
     """Indigo model implementation."""
 
-    def __init__(self, config: Dict[str, Any]):
+    def __init__( 
+            self,
+            num_embeddings: int,  # vocab plus padding and other special tokens
+            d_model: int,
+            num_layers: int,
+            nhead: int,
+            padding_idx: int = 0,
+            dim_feedforward: Optional[int] = None,
+            dropout: float = 0.1,
+            activation: str = "relu",
+            layer_norm_eps: float = 1e-5,
+            rotary_emb_dim: int = 64,
+            max_length: int = 1024,
+            force_flash_attn: bool = False,
+            final_layer_without_normalization: bool = False
+        ):
+
         super().__init__()
         # TODO (URV): Initialize your model components
-        pass
+       
+       # Initialize other things among the two layers below
+       # Get list of IndigoTransformerLayer from IndigoTransformerLayerList here
+       # Get the IndigoTransformerFinalLayer
+       # Get the IndigoTransformerPointerNetwork layer initialized 
+       # get_position would use the IndigoTransformerPointerNetwork, it wont be used in forward
+       # IndigoTransformerLayer, IndigoTransformerLayerList, IndigoTransformerFinalLayer are used in forward
+       
+        
 
     def forward(
         self,
@@ -45,7 +69,49 @@ class IndigoModel(nn.Module):
     ):
         # TODO (URV): Implement your forward pass
         pass
+    
+    def get_position(self):
+        pass
+
+    def get_named_params_for_weight_decay(self):
+        """Get parameters for weight decay (all parameters except biases and layer-norm parameters)."""
+        for name, param in self.named_parameters():
+            if "bias" in name or "norm" in name:
+                continue
+            yield (name, param)
+
+    def get_named_params_for_no_weight_decay(self):
+        """Get parameters for no weight decay (biases and layer-norm parameters)."""
+        for name, param in self.named_parameters():
+            if "bias" in name or "norm" in name:
+                yield (name, param)
 
 
 # endregion: Indigo Transformer
 ########################################################
+
+class IndigoTransformerLayer(nn.Module):
+    def __init__(self):
+        pass
+    def forward(self):
+        pass
+
+class IndigoTransformerLayerList(nn.ModuleList):
+    def __init__(self):
+        pass
+
+    @classmethod
+    def from_layer(self):
+        pass
+
+class IndigoTransformerFinalLayer(nn.Module):
+    def __init__(self):
+        pass
+    def forward(self):
+        pass
+
+class IndigoTransformerPointerNetwork(nn.Module):
+    def __init__(self):
+        pass
+    def forward(self):
+        pass
