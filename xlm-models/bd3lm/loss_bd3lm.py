d299cde573c6c531201a066499780be30214a0ad
Ramcharan Jagalamari <rjagalamari@gmail.com>
2026-08-29 00:17:17 -0400

    Refactor loss calculation to adjust output shape

--- xlm-models/bd3lm/loss_bd3lm.py
+++ xlm-models/bd3lm/loss_bd3lm.py
@@ -105,8 +105,8 @@ class Bd3lmLoss(LossFunction[Bd3lmBatch, Bd3lmLossDict]):
         
         model_output = self._subs_parameterization(logits=logits,
                                       xt=xt)
-        model_output = model_output[:, -target_len:, :]  # [1, 12, 27]
-        loss_mask = loss_mask[:, -target_len:]    # [1, 12]
+        model_output = model_output[:, -target_len:, :]  
+        loss_mask = loss_mask[:, -target_len:]   
         ## when loss_on_padding is False, we want to ignore the PAD tokens in the target_ids
         if not self.loss_on_padding and self.tokenizer.pad_token_id is not None:
             loss_mask = loss_mask * (
